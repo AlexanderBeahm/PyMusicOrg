c16c8119edcfc95eae0582f4712c1e29351840cb
Alex Beahm <alexanderbeahm@gmail.com>
2019-05-28 22:22:45 -0500

    Adding some basic config file loading to environment vars file.

--- EnvironmentVars.py
+++ EnvironmentVars.py
@@ -1,1 +1,21 @@
 #Getter and setters for key environment variables, only accessible via here.
+
+import io
+
+musicLibraryPath = ''
+musicStagingPath = ''
+toBeAddedPath = ''
+
+
+def UpdateEnvironmentPaths(musicLibrary, musicStaging, toBeAdded):
+    musicLibraryPath = musicLibrary
+    musicStagingPath = musicStaging
+    toBeAddedPath = toBeAdded
+    print(musicLibraryPath, musicStagingPath, toBeAddedPath)
+
+def LoadConfigFile():
+    try:
+        configStream = io.open("pymusicorg.config", "r")
+        line = configStream.readline()   
+    except:
+        print("'pymusicorg.config' not found.")
--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
 #Main entry point and music reorg algorithm will be handled here
+import EnvironmentVars
+
 
 while True:
     print("Hi, this is a new program that will perform the following functions...")
     print("1. Unzip and move all folders with audio files to a stagin location")
     print("2. Iterate through all folders, extract metadata")
     print("3. Transfer each album to it's correct folder in music library under '.../Artist/Album/Disc(?)/....")
+    EnvironmentVars.UpdateEnvironmentPaths("A", "B", "C")
+    EnvironmentVars.LoadConfigFile()
     break
